379f758246d12f12310953b7fced70b0fcafddb4
haruki7049 <tontonkirikiri@gmail.com>
2026-01-01 19:30:43 +0900

    Update

--- LeanRiff.lean
+++ LeanRiff.lean
@@ -1,1 +1,11 @@
-def hello := "Hello"
+structure FourCC where
+  data : ByteArray
+  inv : data.size = 4
+
+inductive Chunk where
+  | chunk (id : FourCC) (data : ByteArray)
+  | riff_chunk (id : FourCC) (chunks : Array Chunk)
+  | list_chunk (chunks : Array Chunk)
+
+def Chunk.toByteArray (c : Chunk) : ByteArray :=
+  sorry
