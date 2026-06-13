8527d1fdd9456c7d1e0a6b3932375e8ed359731a
haruki7049 <tontonkirikiri@gmail.com>
2026-06-13 19:27:34 +0900

    Update

--- src/LeanRiff.lean
+++ src/LeanRiff.lean
@@ -8,10 +8,15 @@ def Data.mk (b : ByteArray) (h : b.size = 4 := by decide) : Data :=
 structure FourCC where
   data : Data
 
+def FourCC.fromByteArray (b : ByteArray) (h : b.size = 4 := by decide) : FourCC :=
+  FourCC.mk (Subtype.mk b h)
+
 #check FourCC
 #check FourCC.rec
 #check FourCC.mk
+#check FourCC.fromByteArray
 #check FourCC.data
 #check Data
 #check Data.mk
 #eval Data.mk (ByteArray.mk #[0, 0, 0, 0])
+#check FourCC.fromByteArray (ByteArray.mk #[0, 0, 0, 0])
