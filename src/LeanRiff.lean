e4fe017af8cb786e838d830f112047e1a5348ea4
haruki7049 <tontonkirikiri@gmail.com>
2026-06-13 19:33:13 +0900

    Update

--- src/LeanRiff.lean
+++ src/LeanRiff.lean
@@ -20,3 +20,15 @@ def FourCC.fromByteArray (b : ByteArray) (h : b.size = 4 := by decide) : FourCC 
 #check Data.mk
 #check Data.mk (ByteArray.mk #[0, 0, 0, 0])
 #check FourCC.fromByteArray (ByteArray.mk #[0, 0, 0, 0])
+
+-- RIFF Chunk format
+
+inductive Chunk where
+  | Chunk (four_cc : FourCC) (data : ByteArray)
+  | List (four_cc : FourCC) (chunks : List Chunk)
+  | Riff (four_cc : FourCC) (chunks : List Chunk)
+
+#check Chunk
+#check Chunk.Chunk
+#check Chunk.List
+#check Chunk.Riff
